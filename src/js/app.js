9ed00659d7ffbe591e7d254cfff9e9966e7e9a5a
Kostya Gromov <gromovkostya92@gmail.com>
2018-02-06 10:01:03 +0200

    Added finish functionality

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,31 @@
-import sayHello from './lib/sayHello.js';
+import $ from 'jquery';
+import ImageLoader from './ImageLoader';
 
-sayHello();
+// $('.js-loadme').each(function(index, el) {
+//   let img = new ImageLoader($(el));
+// });
+
+var targets = document.querySelectorAll('.js-loadme');
+
+var options = {
+  rootMargin: '0px',
+  threshold: [0, 1]
+};
+
+var observer = new IntersectionObserver(items => {
+  // console.log(items);
+  items.forEach(el => {
+
+    if (el.isIntersecting && el.intersectionRatio > 0) {
+      if (!$(el.target).hasClass('is-init')) {
+        $(el.target).addClass('is-init');
+        new ImageLoader($(el.target));
+        //console.log('intersected', el.target);
+      }
+    }
+  });
+}, options);
+
+for (var i = 0; i < targets.length; i++) {
+  observer.observe(targets[i]);
+}
